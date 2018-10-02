de9d9b6430aefc7ffa017acc75e78140aafb2195
ThomasFayESS <thomas.fay@esss.se>
2018-10-02 18:48:31 +0200

    startup/llrf.cmd

--- startup/llrf.cmd
+++ startup/llrf.cmd
@@ -2,7 +2,7 @@
 ### LLRF CONTROLLER ###
 #######################
 epicsEnvSet("LLRF_PREFIX"     "$(SIS8300LLRF_PREFIX=LLRF)" )
-epicsEnvSet("LLRF_SLOT"       "$(SIS8300LLRF_SLOT=4)"      )
+epicsEnvSet("LLRF_SLOT"       "$(SIS8300LLRF_SLOT=5)"      )
 epicsEnvSet("LLRF_NOPULSETYPES" "$(SIS8300LLRF_NOPULSETYPES=4)")
 epicsEnvSet("LLRF_PULSETYPE" "$(SIS8300LLRF_PULSETYPE=0)")
 
