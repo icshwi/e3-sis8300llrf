1c5505af1a74b87a9d424a6e38d98c2c0b4b1455
thomasfay <thomas.fay@esss.se>
2019-01-08 16:51:36 +0100

    Added environment log script.

---
+++ envLLRF.bash
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+echo "*** Kernel Release ***"
+uname -a
+echo "*** UDev rules***"
+ls /etc/udev/rles.d/99-*
+echo "*** Kernel Module Load Rules ***"
+ls /etc/modules-load.d/mrf.conf
+ls /etc/modules-load.d/sis8300drv.conf
+echo "*** Kernel Modules Loaded***"
+lsmod | grep sis8300drv
+lsmod | grep mrf
+echo "*** Device Nodes***"
+ls /dev/sis8300-*
+ls /dev/uio*
+echo "*** OS Release***"
+cat /etc/centos-release
+echo "*** Hardware Device ***"
+lspci -tv
+echo "*** EPICS Bases Available***"
+ls /epics/
+echo "*** EPICS Modules Installed ***"
+echo "*** asyn ***"
+make -C ../e3-asyn existent | grep "[0-9\+]\.[0-9\+]\.[0-9\+].*"
+echo "*** loki ***"
+make -C ../e3-loki existent | grep "[0-9\+]\.[0-9\+]\.[0-9\+]"
+echo "*** nds ***"
+make -C ../e3-nds existent | grep "[0-9\+]\.[0-9\+]\.[0-9\+]"
+echo "*** sis8300drv ***"
+make -C ../e3-sis8300drv existent | grep "[0-9\+]\.[0-9\+]\.[0-9\+]"
+echo "*** sis8300 ***"
+make -C ../e3-sis8300 existent | grep "[0-9\+]\.[0-9\+]\.[0-9\+]"
+echo "*** sis8300llrfdrv ***"
+make -C ../e3-sis8300llrfdrv  existent | grep "[0-9\+]\.[0-9\+]\.[0-9\+]"
+echo "*** sis8300llrf ***"
+make existent | grep "[0-9\+]\.[0-9\+]\.[0-9\+]"
+echo "*** mrfioc2 ***"
+make -C ../e3-mrfioc2 existent | grep "[0-9\+]\.[0-9\+]\.[0-9\+]" 
