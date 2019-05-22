1911710837262b711123f0b71f7b24fa8905fb6e
Thomas Fay <thomas.fay@esss.se>
2019-05-22 14:29:31 +0200

    Updates development tools.

--- tools/bumpVer.sh
+++ tools/bumpVer.sh
@@ -1,4 +1,10 @@
-cd ../$1
+if [ $# -ne 4 ]; then
+    echo "usage bumpVers.sh <MODULE PATH> <MODULE DEPENDENCY> <OLD VERSION> <NEW VERSION>"
+    echo "e.g. bumpVers.sh ../e3-sis8300 SIS8300DRV 4.3.3 4.3.4"
+    exit
+fi
+
+cd $1
 oldDep=$(grep $2 configure/CONFIG_MODULE)
 oldVer=$3
 newVer=$4
