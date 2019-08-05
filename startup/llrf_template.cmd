787d45b725be21adac174f970caf0394b5325b77
Gabriel Fedel <gabriel.fedel@esss.se>
2019-08-05 16:01:59 +0200

    Increase EPICS_CA_MAX_ARRAY_BYTES
    This allow the large array to pass through network

--- startup/llrf_template.cmd
+++ startup/llrf_template.cmd
@@ -1,7 +1,7 @@
 # Timing Configuration
 iocshLoad $(E3_CMD_TOP)/timing.iocsh
 
-epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES" "8388624")
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES" "16777300")
 require sis8300llrf, 3.10.3
 ndsSetTraceLevel 3
 
