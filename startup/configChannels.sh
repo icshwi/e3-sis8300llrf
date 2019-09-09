a8e4b73bdf503efa6615df196f346261c96e9c9a
Gabriel Fedel <gabriel.fedel@esss.se>
2019-09-09 15:47:24 +0200

    Change configChannels to use channel number from input file
    And add a first line on input file to describe the file

--- startup/configChannels.sh
+++ startup/configChannels.sh
@@ -39,35 +39,36 @@ template0="dbLoadRecords(\$(E3_CMD_TOP)/alias.template, \"O=$LLRF_IOC_NAME$iNum:
 template1="dbLoadRecords(\$(E3_CMD_TOP)/desc.template, "
 
 
-i=0
-cat $confFile | while read line
+{
+read
+while IFS=, read -r ch pv_name desc
 do
-    pv_name=$(echo $line | cut -f 1 -d ,)
-    desc=$(echo $line | cut -f 2 -d , | sed -e 's/^[ \t]*//')
+    ch=$(echo $ch |  sed -e 's/^[ \t]*//')
+    pv_name=$(echo $pv_name |  sed -e 's/^[ \t]*//')
+    desc=$(echo $desc |  sed -e 's/^[ \t]*//')
     if [ ${#desc} -gt 41 ]; then
         echo "Warning: Description truncated to char[41] - ${desc:0:40}"
         desc=${desc:0:40}
     fi
 
     # set aliases
-    echo "$template0$i, A=$LLRF_IOC_NAME$iNum:$pv_name\")" >> $fPath
-    echo "$template0$i-ATT, A=$LLRF_IOC_NAME$iNum:$pv_name-ATT\")" >> $fPath
-    echo "$template0$i-ATT-RBV, A=$LLRF_IOC_NAME$iNum:$pv_name-ATT-RBV\")" >> $fPath
-    echo "$template0$i-DECF, A=$LLRF_IOC_NAME$iNum:$pv_name-DECF\")" >> $fPath
-    echo "$template0$i-DECF-RBV, A=$LLRF_IOC_NAME$iNum:$pv_name-DECF-RBV\")" >> $fPath
-    echo "$template0$i-DECO, A=$LLRF_IOC_NAME$iNum:$pv_name-DECO\")" >> $fPath
-    echo "$template0$i-DECO-RBV, A=$LLRF_IOC_NAME$iNum:$pv_name-DECO-RBV\")" >> $fPath
-    echo "$template0$i-FileName, A=$LLRF_IOC_NAME$iNum:$pv_name-FileName\")" >> $fPath
-    echo "$template0$i-Slope, A=$LLRF_IOC_NAME$iNum:$pv_name-Slope\")" >> $fPath
-    echo "$template0$i-Offset, A=$LLRF_IOC_NAME$iNum:$pv_name-Offset\")" >> $fPath
-    echo "$template0$i-InputValues, A=$LLRF_IOC_NAME$iNum:$pv_name-InputValues\")" >> $fPath
-    echo "$template0$i-DigitisedValues, A=$LLRF_IOC_NAME$iNum:$pv_name-DigitisedValues\")" >> $fPath
-    echo "$template0$i-FittedLine, A=$LLRF_IOC_NAME$iNum:$pv_name-FittedLine\")" >> $fPath
+    echo "$template0$ch, A=$LLRF_IOC_NAME$iNum:$pv_name\")" >> $fPath
+    echo "$template0$ch-ATT, A=$LLRF_IOC_NAME$iNum:$pv_name-ATT\")" >> $fPath
+    echo "$template0$ch-ATT-RBV, A=$LLRF_IOC_NAME$iNum:$pv_name-ATT-RBV\")" >> $fPath
+    echo "$template0$ch-DECF, A=$LLRF_IOC_NAME$iNum:$pv_name-DECF\")" >> $fPath
+    echo "$template0$ch-DECF-RBV, A=$LLRF_IOC_NAME$iNum:$pv_name-DECF-RBV\")" >> $fPath
+    echo "$template0$ch-DECO, A=$LLRF_IOC_NAME$iNum:$pv_name-DECO\")" >> $fPath
+    echo "$template0$ch-DECO-RBV, A=$LLRF_IOC_NAME$iNum:$pv_name-DECO-RBV\")" >> $fPath
+    echo "$template0$ch-FileName, A=$LLRF_IOC_NAME$iNum:$pv_name-FileName\")" >> $fPath
+    echo "$template0$ch-Slope, A=$LLRF_IOC_NAME$iNum:$pv_name-Slope\")" >> $fPath
+    echo "$template0$ch-Offset, A=$LLRF_IOC_NAME$iNum:$pv_name-Offset\")" >> $fPath
+    echo "$template0$ch-InputValues, A=$LLRF_IOC_NAME$iNum:$pv_name-InputValues\")" >> $fPath
+    echo "$template0$ch-DigitisedValues, A=$LLRF_IOC_NAME$iNum:$pv_name-DigitisedValues\")" >> $fPath
+    echo "$template0$ch-FittedLine, A=$LLRF_IOC_NAME$iNum:$pv_name-FittedLine\")" >> $fPath
 
     # set descriptions
     echo "$template1\"A=$LLRF_IOC_NAME$iNum:$pv_name, D=$desc\")" >> $fPathDesc
 
-    i=$(($i+1))
 done
-
+} < $confFile
 
